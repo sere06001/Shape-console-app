364bd9dbb669dc6db0c3f059cfabcaca26248b6f
sere06001 <sere06001@utb.vaxjo.se>
2024-08-27 14:08:04 +0200

    Add project files.

---
+++ Shape console app/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Shape_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Shape console app/Shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_console_app
+{
+    internal class Shape
+    {
+        protected double width;
+        protected double height;
+
+        public Shape (double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+    }
+
+    internal class Triangle : Shape
+    {
+        protected double width;
+        protected double height;
+
+        public Triangle(double w, double h) : base (w, h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public double Area (double width, double height)
+        {
+            return (width * height)/2;
+        }
+
+        public double Circumference (double width, double height)
+        {
+            double hypotenuse = Math.Sqrt(width*width + height*height);
+            double sum = hypotenuse + width + height;
+            return sum;
+        }
+    }
+
+    internal class Rectangle : Shape
+    {
+        protected double width;
+        protected double height;
+
+        public Rectangle (double w, double h) : base(w, h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public double Area (double width, double height)
+        {
+            return width * height;
+        }
+
+        public double Circumference(double width, double height)
+        {
+            double sum = (width * 2) + (height * 2);
+            return sum;
+        }
+    }
+}
