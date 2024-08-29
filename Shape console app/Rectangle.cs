f97b60ded185af456d707ea3a3745ea10440eca0
sere06001 <sere06001@utb.vaxjo.se>
2024-08-29 13:41:54 +0200

    fixed

---
+++ Shape console app/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_console_app
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(double w, double h) : base(w, h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public double Area(double width, double height)
+        {
+            return width * height;
+        }
+
+        public double Circumference(double width, double height)
+        {
+            double sum = (width * 2) + (height * 2);
+            return sum;
+        }
+    }
+}
---
+++ Shape console app/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_console_app
+{
+    internal class Triangle : Shape
+    {
+        public Triangle(double w, double h) : base(w, h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public double Area(double width, double height)
+        {
+            return (width * height) / 2;
+        }
+
+        public double Circumference(double width, double height)
+        {
+            double hypotenuse = Math.Sqrt(width * width + height * height);
+            double sum = hypotenuse + width + height;
+            return sum;
+        }
+    }
+}
